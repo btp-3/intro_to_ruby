a6918c4861a6ac5ba1648464fa69d764ebf84c86
Byron Pearce <btpearce4@gmail.com>
2020-10-07 22:22:41 -0300

    Added small sections to get first/last name from user

--- name.rb
+++ name.rb
@@ -4,3 +4,17 @@ puts "Hello! What is your name?"
 name = gets.chomp
 
 puts "Hi, #{name}. Nice to meet you!"
+
+# section to print user name 10 times
+
+10.times { puts name }
+
+# ask user for first & last name and save to var
+
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Hello there, #{first_name} #{last_name}. Nice to meet you!"
